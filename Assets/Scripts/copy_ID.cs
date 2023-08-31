48bcd1db4ec8ffb0c770dc1a98efeab398e530f7
Audrey Nguyen <hoangsa9898@gmail.com>
2023-08-31 01:54:27 -0700

    added id_copied status

--- Assets/Scripts/copy_ID.cs
+++ Assets/Scripts/copy_ID.cs
@@ -8,18 +8,21 @@ public class copy_ID : MonoBehaviour
     public game_data game_data;
     private Renderer render;
     private new Collider collider;
+    public GameObject status;
 
     private void Start()
     {
         // get render component
         render = GetComponent<Renderer>();
         collider = GetComponent<Collider>();
+        status.SetActive(false);
     }
 
     public void CopyToClipboard()
     {
-        GUIUtility.systemCopyBuffer = game_data.userID;
-        Debug.Log("Unique ID copied to clipboard: " + game_data.userID);
+        GUIUtility.systemCopyBuffer = game_data.userID.ToString();
+        Debug.Log("Unique ID copied to clipboard: " + game_data.userID.ToString());
+        status.SetActive(true);
     }
 
     void Update()
