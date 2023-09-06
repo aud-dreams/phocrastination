39b6ce9882260b03a81b46da7ce03b13c1f3d409
Audrey Nguyen <hoangsa9898@gmail.com>
2023-09-05 18:51:04 -0700

    working on userid, reset to previous commit

--- Assets/Scripts/generate_id.cs
+++ Assets/Scripts/generate_id.cs
@@ -1,77 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 using Proyecto26;
 
-[System.Serializable]
-public class UserData
-{
-    public Dictionary<string, UserDataInstance> users;
-}
-
-[System.Serializable]
-public class UserDataInstance
-{
-    public double bowl_created_ts1;
-    public double bowl_created_ts2;
-    public int bowl_washed_rem;
-    public int bowl_washed_tot;
-    public double bowl_washed_ts1;
-    public double bowl_washed_ts2;
-    public double cat_cue_ts1;
-    public double cat_cue_ts2;
-    public double cat_scene_ts1;
-    public double cat_scene_ts2;
-    public int color_switch;
-    public int distractability_bool;
-    public int game_status;
-    public double global_timer;
-    public int order_collected_rem;
-    public int order_collected_tot;
-    public double order_collected_ts1;
-    public double order_collected_ts2;
-    public int order_given_rem;
-    public int order_given_tot;
-    public double order_given_ts1;
-    public double order_given_ts2;
-    public float ratio_hit;
-    public int redo;
-    public double total_time_drawing;
-}
-
 public class generate_id : MonoBehaviour
 {
     public game_data game_data;
+    Counter UserCount;
 
     void Start()
     {
-        Debug.Log("Start");
-        StartCoroutine(generateUserID());
-        Debug.Log("After");
+        GetNumberOfUsers();
     }
 
-    IEnumerator generateUserID()
+    private void GetNumberOfUsers()
     {
-        RestClient.Get(game_data.db_url).Then(response =>
+        RestClient.Get("https://phocrastination-user-default-rtdb.firebaseio.com/test.json").Then(response =>
         {
-            if (!string.IsNullOrEmpty(response.Text))
-            {
-                UserData jsonData = JsonUtility.FromJson<UserData>(response.Text);
+            Debug.Log(response.ToString());
 
-                int instanceCount = jsonData.users.Count;
-                int userID = instanceCount + 1;
+            // // create Counter instance
+            // UserCount = new Counter(response.user_count);
+            // UserCount.add();
+            // game_data.userID = UserCount.user_count;
 
-                game_data.userID = userID;
-                Debug.Log($"Generated User ID: {userID}");
-            }
+            // // increment and update database
+            // RestClient.Put("https://phocrastination-user-default-rtdb.firebaseio.com/test.json", UserCount);
+            // Debug.Log(game_data.userID);
         });
-
-        yield return null;
     }
 
-    void Update()
+    public class Counter
     {
-        
+        public int user_count { get; set; }
+
+        public Counter()
+        {
+        }
+
+        public Counter(int initial)
+        {
+            user_count = initial;
+        }
+
+        public void add()
+        {
+            user_count++;
+        }
     }
 }
